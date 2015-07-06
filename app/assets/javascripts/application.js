bd93c436c5a2d6a37a4f61a5f3957df6559ae51a
MMRahman28 <mashuk.pro@gmail.com>
2015-07-06 08:12:22 -0400

    without turbolink

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require unslider
-//= require turbolinks
-//= require_tree .
 //= require app.js
+//= require_tree .
